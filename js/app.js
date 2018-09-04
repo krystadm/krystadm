a8d16e9f204acd16f1c30f250ecc295b22dd0dfc
krystadm <42557138+krystadm@users.noreply.github.com>
2018-09-03 22:21:20 -0700

    updated

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation()
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
