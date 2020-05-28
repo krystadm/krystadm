729ee56806b7f31e7e042e2dd6997c912f3796ca
krystadm <krystamadewell@gmail.com>
2020-05-28 16:46:22 -0400

    sticky

--- js/app.js
+++ js/app.js
@@ -56,3 +56,9 @@ $(function() {
 
 
       new WOW().init();
+
+      var stickyElements = document.getElementsByClassName('sticky');
+
+      for (var i = stickyElements.length - 1; i >= 0; i--) {
+          Stickyfill.add(stickyElements[i]);
+      }
