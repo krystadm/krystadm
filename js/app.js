4d360d159befefffaf243580e2b796af6058365c
krystadm <krystamadewell@gmail.com>
2020-10-15 19:12:34 -0400

    redesigned again

--- js/app.js
+++ js/app.js
@@ -90,3 +90,19 @@ jQuery(function($) {
   $(window).trigger('scroll');
 
 });
+
+// back to top
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
